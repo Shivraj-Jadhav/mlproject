8372a136b00ba66ba7069f1f3c9417c056e475fb
Shivraj Jadhav <69976088+Shivraj-Jadhav@users.noreply.github.com>
2024-08-17 23:54:20 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,7 @@ from src.exception import CustomException
 
 import numpy as np
 import pandas as pd
-import dill
+import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
@@ -48,11 +48,28 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,param):
         raise CustomException(e,sys)
     
 
+import pickle  # Or dill if needed
 
 def load_object(file_path):
+    """Loads an object from a file.
+
+    Args:
+        file_path: The path to the file containing the object.
+
+    Returns:
+        The loaded object.
+
+    Raises:
+        FileNotFoundError: If the file does not exist.
+        pickle.UnpicklingError: If there's an error unpickling the object.
+    """
+
     try:
         with open(file_path, "rb") as file_obj:
-            return dill.load(file_obj)
-        
-    except Exception as e:
-        raise CustomException(e,sys)
+            return pickle.load(file_obj)
+    except FileNotFoundError as e:
+        print(f"Error: File not found: {file_path}")
+        raise e
+    except pickle.UnpicklingError as e:
+        print(f"Error unpickling object from {file_path}")
+        raise e
